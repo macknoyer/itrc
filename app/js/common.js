6e87492781ae5a2d7480283c79dac0169de86008
judns <judns@hismith.ru>
2018-04-08 02:35:54 +0500

    metroline

---
+++ app/js/common.js
@@ -0,0 +1,11 @@
+$(function() {
+
+	$(document).ready(function() {
+    $(".slider_index_top").owlCarousel({
+    	items:1,
+    	dots: true,
+    	dotsContainer: '#customDots'
+    });
+    });
+
+});
